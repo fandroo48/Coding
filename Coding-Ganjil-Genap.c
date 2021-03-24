a6a501404245ad977dcd97978e06d6a731fdb21f
Vibssss <71185700+fandroo48@users.noreply.github.com>
2021-03-24 22:04:27 +0700

    percobaan kedua

---
+++ Coding-Ganjil-Genap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i,a;
+  printf("masukan bilangan : ");
+    scanf("%d",&a);
+
+  printf("\nbilangan ganjil-genap dari 0 hingga %d adalah :\n",a);
+
+    printf("Genap\tGanjil\n");
+
+  for (i=0; i<=a; i++){
+    printf("%d\t",i);
+    i++;
+  if(i<=a)
+      printf("%d\n",i);
+  }
+}
